358ebe016a9283054170e0196c7fc1b538f45cbc
Stangenzirkel4 <113057495+Stangenzirkel4@users.noreply.github.com>
2023-05-20 11:57:36 +0300

    Lab is done
    Everything is done

--- start.py
+++ start.py
@@ -2,44 +2,51 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5432@localhost'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5432@localhost/Lab_9'
 db = SQLAlchemy(app)
 
-class Message(db.Model):
+class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(1024), nullable=False)
-    def __init__(self,text,tags):
-        self.text = text.strip()
-        self.tags = [
-            Tag(text=tag.strip()) for tag in tags.split(',')
+    company_name = db.Column(db.String(256), nullable=False)
+    def __init__(self, company, term):
+        self.company_name = company.strip()
+        self.term = [
+            Term(duration=int(t)) for t in term.split(',')
         ]
 
-class Tag(db.Model):
+class Term(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(32), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
 
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
-    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    company = db.relationship('Company', backref=db.backref('term'))
 
 with app.app_context():
     db.create_all()
 
 @app.route("/", methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('main.html', companies=Company.query.all())
 
 
 @app.route("/main", methods=['GET'])
 def main():
-    return render_template('main.html', messages=Message.query.all())
+
+    return render_template('main.html', companies=Company.query.all())
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = request.form['text']
-    tag = request.form['tag']
+@app.route('/add_experience', methods=['POST'])
+def add_experience():
+    company = request.form['company']
+    term = request.form['term']
 
-    db.session.add(Message(text, tag))
+    db.session.add(Company(company, term))
     db.session.commit()
 
+    return redirect(url_for('main'))
+@app.route('/clear_db', methods=['POST'])
+def clear_db():
+    db.session.query(Term).delete()
+    db.session.query(Company).delete()
+    db.session.commit()
     return redirect(url_for('main'))
